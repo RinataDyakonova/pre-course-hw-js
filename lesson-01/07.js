519ca56b1ade666eea19decc31d770a432f55602
Rinata21 <djakonovarinata@gmail.com>
2023-07-27 00:01:34 +0500

    07 js problem

--- lesson-01/07.js
+++ lesson-01/07.js
@@ -1,4 +1,4 @@
-let iAm = 'я';
+​let iAm = 'я';
 let krasavchik = "красавчик!";
-console.log(iAm.charAt(0).toUpperCase() + iAm.slice(1) + ' ' + krasavchik);
+console.log(iAm.toUpperCase() + ' ' + krasavchik);
 
